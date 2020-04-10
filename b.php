5153fc775d16d5bed1c6158edecd9fbc8a017b96
Soufiane Ghzal <sghzal@gmail.com>
2020-04-10 14:21:43 +0200

    Update b.php

--- b.php
+++ b.php
@@ -1,3 +1,10 @@
-<?php
-
-header('Location: /c.php');    
+<html>
+  <head>
+    <title>test</title>
+  </head>
+  <body>
+    <a href="/index.php">index</a>
+    
+    <a href="/a.php">a</b>
+  </body>
+</html>
