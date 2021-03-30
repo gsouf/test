8d42b70d992f86368b6d7446ab1232499ee5f354
Soufiane Ghzal <sghzal@gmail.com>
2021-03-30 09:26:40 +0200

    Update a.php

--- a.php
+++ a.php
@@ -3,10 +3,6 @@
     <title>test</title>
   </head>
   <body>
-    <a href="/index.php">index</a>
-    
-    <a href="/c.php">c1</a>
-        
-    <a href="/c.php">c2</a>
+    <a href="/c.php">index</a>
   </body>
 </html>
