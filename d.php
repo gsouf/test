741f2ef1f4b20ceae20d211ff4ede95ecb068450
Soufiane Ghzal <sghzal@gmail.com>
2021-05-12 11:11:48 +0200

    Create d.php

---
+++ d.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <title>test</title>
+  </head>
+  <body>
+    two words
+    
+    <div style="visibility:hidden; opacity: 0">
+        invisible text
+        <div style="display:none">
+          not displayed text
+        </div>
+      <div>
+    
+    
+  </body>
+</html>
